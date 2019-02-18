83c6d68f95af20ae14c8eb880042a2c955085109
dominikdeimel <d.deimel@field-interactive.com>
2019-02-18 16:04:58 +0100

    added a jQuery like alias for document.querySelector() and document.querySelectorAll()

---
+++ js/helper/querySelectorAlias.js
@@ -0,0 +1,18 @@
+/**
+ * jQuery like Syntax for document.querySelector() or document.querySelectorAll()
+ * @param {NodeSelector} domNodes
+ * @return {Node | Array}
+ */
+
+const $ = domNodes => {
+    if (typeof jQuery == 'undefined') {
+        const items = document.querySelectorAll(domNodes)
+        if (items.length > 1) {
+            return Array.from(items)
+        } else if (items.length === 1) {
+            return items[0]
+        }
+    }
+}
+
+export { $ }
