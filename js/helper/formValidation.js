799d9918081d9bfcf375f73e40389e07cca1f607
dominikdeimel <d.deimel@field-interactive.com>
2019-02-18 16:15:31 +0100

    changes Description for Function params and inserted querySelector alias

--- js/helper/formValidation.js
+++ js/helper/formValidation.js
@@ -1,16 +1,18 @@
+import { $ } from './querySelectorAlias'
+
 /**
  *
- * @param {NodeSelector} form
- * @param {NodeSelector} required
- * @param {NodeSelector} button
- * @param {NodeSelector} dsgvo
+ * @param {NodeSelector} [form=document.querySelector('form')] - Selector for the Form
+ * @param {NodeSelector} [required=document.querySelectorAll('[required]')] - Selector for all fields with the required attribute
+ * @param {NodeSelector} [button=document.querySelector('form button[type="submit"]')] - Selector for submit button
+ * @param {NodeSelector} [dsgvo=document.querySelector('#form_DSGVO')] - Selector for DSGVO Checkbox
  */
 
 const formValidation = (
-    form = document.querySelector('form'),
-    required = document.querySelectorAll('[required]'),
-    button = document.querySelector('form button[type="submit"]'),
-    dsgvo = document.querySelector('#form_DSGVO')
+    form = $('form'),
+    required = $('[required]'),
+    button = $('form button[type="submit"]'),
+    dsgvo = $('#form_DSGVO')
 ) => {
     if (form) {
         form.addEventListener('click', () => {
