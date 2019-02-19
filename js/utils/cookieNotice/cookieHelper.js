c18ec2e4743f4588c3f088fc97962e413d38f199
dominikdeimel <d.deimel@field-interactive.com>
2019-02-19 09:41:34 +0100

    added Params

--- js/utils/cookieNotice/cookieHelper.js
+++ js/utils/cookieNotice/cookieHelper.js
@@ -1,13 +1,31 @@
+/**
+ *
+ * @param {String} name
+ * @param {String} value
+ * @param {Number} days
+ */
+
 const setCookie = (name, value, days) => {
     const d = new Date
     d.setTime(d.getTime() + 24 * 60 * 60 * 1000 * days)
     document.cookie = `${name}=${value};path=/;expires=${d.toUTCString()}`
 }
 
+/**
+ *
+ * @param {String} name
+ * @return {any}
+ */
+
 const getCookie = name => {
     const v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)')
     return v ? v[2] : null
 }
+
+/**
+ *
+ * @param {String} name
+ */
 
 const deleteCookie = name => setCookie(name, '', -1)
 
