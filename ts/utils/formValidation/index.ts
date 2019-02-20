0366ae505ddd6057aca8d0e04631483d862d0068
dominikdeimel <d.deimel@field-interactive.com>
2019-02-20 16:27:44 +0100

    added correct types for Formvalidation

--- ts/utils/formValidation/index.ts
+++ ts/utils/formValidation/index.ts
@@ -4,14 +4,19 @@
  * @param required
  * @param button
  * @param dsgvo
+ *
  */
 
-const formValidation = (
-    form: HTMLELement = document.querySelector('form'),
-    required: NodeList = document.querySelectorAll('[required]'),
-    button: HTMLELement = document.querySelector('form button[type="submit"]'),
-    dsgvo: HTMLELement = document.querySelector('#form_DSGVO')
-): void => {
+interface Form {
+    (form?: HTMLElement, required?: NodeList, button?: HTMLElement, dsgvo?: HTMLInputElement): void
+}
+
+const formValidation: Form = (
+    form = document.querySelector('form'),
+    required = document.querySelectorAll('[required]'),
+    button = document.querySelector('form button[type="submit"]'),
+    dsgvo = document.querySelector('#form_DSGVO')
+) => {
     if (form) {
         form.addEventListener('click', (): void => {
             if (validateForm() && dsgvo.checked) {
@@ -24,7 +29,7 @@ const formValidation = (
 
     const validateForm = (): boolean => {
         let isValid: boolean = true;
-        required.forEach((i: HTMLELement): void => {
+        required.forEach((i: HTMLInputElement): void => {
             if (i.value === '') isValid = false
         });
         return isValid
